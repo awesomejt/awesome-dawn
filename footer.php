ca5f5a9dc7aadfc6f62a84d71c749c75a0c00711
Jason Taylor <jason@screencasts.pro>
2015-08-16 04:49:15 -0400

    initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Awesome Dawn
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'awesome-dawn' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'awesome-dawn' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'awesome-dawn' ), 'awesome-dawn', '<a href="http://jtaylor.pro" rel="designer">Jason Taylor</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
